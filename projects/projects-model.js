f0a4afb9c7b3de0f10a0a532df49572135e7c0d8
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-02-08 10:22:00 -0800

    get tasks working

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -3,10 +3,17 @@ const db = require('../data/db-config.js');
 module.exports = {
     find,
     findResources,
-    // findById,
+    findTasks,
+    findById,
     // getInstructions,
     // getAllInstructions,
 };
+
+function findById(id) {
+    return db('projects')
+      .where({ id })
+      .first();
+  }
 
 function find() {
     return db('projects')
@@ -15,3 +22,16 @@ function find() {
 function findResources() {
     return db('resources')
 }
+
+function findTasks() {
+    return db('tasks as t')
+        .join('projects as p', 'p.id', 't.project_id')
+        .select('p.project_name', 'p.project_description', 't.task_description', 't.task_notes', 't.task_completed')
+}
+
+// function findTasks(project_id) {
+//     return db('projects as p') 
+//         .join('tasks as t', 'p.id', 't.project_id')
+//         .select('p.project_name', 'p.project_description', 't.task_description', 't.task_notes', 't.task_completed')
+//         .where('p.id', project_id)
+// }
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     Projects.find()
-          .then(posts => {
-              res.status(200).json(posts);
+          .then(projects => {
+              res.status(200).json(projects);
           })
           .catch(err => {
               res.status(500).json({ error: "The projects information could not be retrieved." });
@@ -16,8 +16,8 @@ router.get('/', (req, res) => {
 
 router.get('/resources', (req, res) => {
     Projects.findResources()
-          .then(posts => {
-              res.status(200).json(posts);
+          .then(resources => {
+              res.status(200).json(resources);
           })
           .catch(err => {
               res.status(500).json({ error: "The resource information could not be retrieved." });
@@ -25,5 +25,43 @@ router.get('/resources', (req, res) => {
 });
 
 
+router.get('/tasks', (req, res) => {
+    Projects.findTasks()
+          .then(resources => {
+              res.status(200).json(resources);
+          })
+          .catch(err => {
+              res.status(500).json({ error: "The task information could not be retrieved." });
+          })
+});
+
+// custom middleware
+
+function validateProjectId(req, res, next) {
+    const {id} = req.params;
+    Projects.getById(id)
+      .then(project => {
+        if(project) {
+          req.project = project;
+          next();
+        } else {
+          res.status(400).json({ message: "invalid project id" });
+        }   
+      })
+      .catch(err => {
+        res.status(500).json({message: 'exception error'});
+      })
+  }
+  
+//   function validateUser(req, res, next) {
+//     const projectData = req.body;
+//     if(!projectData) {
+//       res.status(400).json({ message: "missing user data" });
+//     } else if (!projectData.text) {
+//       res.status(400).json({ message: 'missing required text field'})
+//     } else {
+//       next();
+//     }
+//   }
 
 module.exports = router;
